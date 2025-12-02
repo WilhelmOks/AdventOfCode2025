06e9f714f7259558673ece5c8f6523d23fc5de64
Wilhelm Oks <oksw@Mac.fritz.box>
2025-12-02 22:23:40 +0100

    day 2 part 2

--- AdventOfCode2025/Days/Day02.swift
+++ AdventOfCode2025/Days/Day02.swift
@@ -14,6 +14,54 @@ private func parse(from input: String) -> [ClosedRange<Int>] {
     }
 }
 
+private var factorsCache: [Int: [Int]] = [:]
+
+private func factors(of n: Int) -> [Int] {
+    precondition(n > 0, "n must be positive")
+    
+    if let cached = factorsCache[n] {
+        return cached
+    }
+    
+    let sqrtn = Int(Double(n).squareRoot())
+    var factors: [Int] = []
+    factors.reserveCapacity(2 * sqrtn)
+    for i in 1...sqrtn {
+        if n % i == 0 {
+            factors.append(i)
+        }
+    }
+    var j = factors.count - 1
+    if factors[j] * factors[j] == n {
+        j -= 1
+    }
+    while j >= 0 {
+        factors.append(n / factors[j])
+        j -= 1
+    }
+    
+    factorsCache[n] = factors
+    
+    return factors
+}
+
+private func splitEvenly(_ id: String, into parts: Int) -> [String] {
+    if id.count.isMultiple(of: parts) {
+        return (0..<parts).map { i in
+            let partLength = id.count / parts
+            let from = id.index(id.startIndex, offsetBy: i * partLength)
+            let to = id.index(id.startIndex, offsetBy: partLength + i * partLength)
+            return String(id[from..<to])
+        }
+    } else {
+        return []
+    }
+}
+
+private func allEqual(parts: [String]) -> Bool {
+    Set(parts).count == 1
+}
+
 struct Day02: View {
     var body: some View {
         content()
@@ -21,7 +69,7 @@ struct Day02: View {
     
     @ViewBuilder private func content() -> some View {
         ResultView(
-            day: 1,
+            day: 2,
             calc1: calculate1,
             calc2: calculate2,
         )
@@ -60,8 +108,34 @@ struct Day02: View {
     
     func calculate2() async -> String {
         let input = readInput(day: "02")
+        let ranges = parse(from: input)
         
-        return "?"
+        var invalidIds: [Int] = []
+        
+        for range in ranges {
+            for number in range {
+                let id = String(number)
+                guard let intId = Int(id) else {
+                    fatalError("Coudn't convert to Int: \(id)")
+                }
+
+                let factors = factors(of: id.count).dropFirst()
+                
+                for factor in factors {
+                    let parts = splitEvenly(id, into: Int(factor))
+                    if parts.count >= 2 {
+                        if allEqual(parts: parts) {
+                            invalidIds.append(intId)
+                            break
+                        }
+                    }
+                }
+            }
+        }
+        
+        let sum = invalidIds.reduce(0, +)
+        
+        return String(sum)
     }
 }
 
--- AdventOfCode2025/UI/ContentView.swift
+++ AdventOfCode2025/UI/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Day01()
+            Day02()
         }
         .padding()
     }
