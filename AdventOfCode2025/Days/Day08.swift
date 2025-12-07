7ed7a8bbae3faebe538502aba2c406498a2ed958
Wilhelm Oks <oksw@Mac.fritz.box>
2025-12-07 13:44:07 +0100

    added blank day 8

---
+++ AdventOfCode2025/Days/Day08.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+
+struct Day08: View {
+    var body: some View {
+        content()
+    }
+    
+    @ViewBuilder private func content() -> some View {
+        ResultView(
+            day: 8,
+            calc1: calculate1,
+            calc2: calculate2,
+        )
+        .padding()
+    }
+    
+    func calculate1() async -> String {
+        let input = readInput(day: "08")
+        
+        return String("?")
+    }
+    
+    func calculate2() async -> String {
+        let input = readInput(day: "08")
+        
+        return String("??")
+    }
+}
+
+#Preview {
+    Day08()
+}
