4fe58b6eac8e4fc44cd409a31df5a4f3acd6498e
Wilhelm Oks <oksw@Mac.fritz.box>
2025-12-11 18:10:18 +0100

    day 11 part 2 (incomplete)

--- AdventOfCode2025/Days/Day11.swift
+++ AdventOfCode2025/Days/Day11.swift
@@ -1,42 +1,78 @@
 import SwiftUI
 
 private enum PathElement: CustomStringConvertible {
-    case node(_ elements: [PathElement])
-    case leaf
+    case node(_ id: String, _ elements: [PathElement], _ passedNodesInfo: PassedNodesInfo)
+    case leaf(isOut: Bool, _ passedNodesInfo: PassedNodesInfo)
     
-    init(id: String, devices: [String: Set<String>]) {
+    init(id: String, devices: [String: Set<String>], passedNodesInfo: PassedNodesInfo) {
+        var changedInfo = passedNodesInfo
+        changedInfo.visited.insert(id)
+        if id == "dac" {
+            changedInfo.dac = true
+        } else if id == "fft" {
+            changedInfo.fft = true
+        }
+        
         switch id {
         case "out":
-            self = .leaf
+            self = .leaf(isOut: true, changedInfo)
+        case let visitedId where passedNodesInfo.visited.contains(visitedId):
+            self = .leaf(isOut: false, changedInfo)
         default:
-            if let outputIds = devices[id] {
-                let nodes = outputIds.map { outputId in
-                    PathElement(id: outputId, devices: devices)
+            if let outputIds = devices[id]?.subtracting(changedInfo.visited) {
+                let nodes: [Self] = outputIds.compactMap { outputId in
+                    //print("BUILDING id:\(id), outputId:\(outputId), visited: \(passedNodesInfo.visited)")
+                    if passedNodesInfo.visited.contains(outputId) {
+                        //return PathElement.leaf(isOut: false, changedInfo)
+                        return nil
+                    } else {
+                        return PathElement(id: outputId, devices: devices, passedNodesInfo: changedInfo)
+                    }
                 }
-                self = .node(nodes)
+                if nodes.isEmpty {
+                    self = .leaf(isOut: false, changedInfo)
+                } else {
+                    self = .node(id, nodes, changedInfo)
+                }
             } else {
-                fatalError("no subpaths for \(id)")
+                fatalError("no outputs for \(id)")
             }
         }
     }
     
     var description: String {
         switch self {
-        case .node(let elements):
-            return "(\(elements.map(\.description).joined(separator: " ")))"
+        case .node(let id, let elements, _):
+            //return "\(id) \(elements.map(\.description).joined(separator: " "))"
+            return "\(id) \(elements.reduce("") { $0 + $1.description })"
         case .leaf:
-            return "out"
+            return "out\n"
         }
     }
     
     var numberOfPathsToOut: Int {
         switch self {
-        case .node(let elements):
+        case .node(_, let elements, _):
             return elements.reduce(0) { $0 + $1.numberOfPathsToOut }
-        case .leaf:
-            return 1
+        case .leaf(let isOut, _):
+            return isOut ? 1 : 0
         }
     }
+    
+    var numberOfPathsToOutPassingPart2Requirements: Int {
+        switch self {
+        case .node(_, let elements, _):
+            return elements.reduce(0) { $0 + $1.numberOfPathsToOutPassingPart2Requirements }
+        case .leaf(let isOut, let info):
+            return isOut && info.dac && info.fft ? 1 : 0
+        }
+    }
+}
+
+private struct PassedNodesInfo: Hashable {
+    var fft: Bool
+    var dac: Bool
+    var visited: Set<String> = []
 }
 
 private func parseInput(_ input: String) -> [String: Set<String>] {
@@ -66,6 +102,8 @@ struct Day11: View {
     }
     
     func calculate1() async -> String {
+        return "-"
+        
         let input = readInput(day: "11")
         /*
         let input = """
@@ -86,7 +124,7 @@ struct Day11: View {
         
         //print(devices.map { "\($0)" }.joined(separator: "\n"))
         
-        let path = PathElement(id: "you", devices: devices)
+        let path = PathElement(id: "you", devices: devices, passedNodesInfo: .init(fft: false, dac: false))
         
         //print(path)
         
@@ -96,9 +134,39 @@ struct Day11: View {
     }
     
     func calculate2() async -> String {
-        let input = readInput(day: "11")
+        //return "--"
         
-        return String("??")
+        let input = readInput(day: "11")
+        /*
+        let input = """
+            svr: aaa bbb
+            aaa: fft
+            fft: ccc
+            bbb: tty
+            tty: ccc
+            ccc: ddd eee
+            ddd: hub
+            hub: fff
+            eee: dac
+            dac: fff
+            fff: ggg hhh
+            ggg: out
+            hhh: out
+            """
+         */
+        
+        let devices = parseInput(input)
+        
+        let path = PathElement(id: "svr", devices: devices, passedNodesInfo: .init(fft: false, dac: false))
+        
+        print(path)
+        
+        let result = path.numberOfPathsToOutPassingPart2Requirements
+        //let result = path.numberOfPathsToOut
+        
+        // part 2 is incomplete: endless loop in PathElement.init()
+        
+        return String(result)
     }
 }
 
--- AdventOfCode2025/UI/ContentView.swift
+++ AdventOfCode2025/UI/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Day10()
+            Day11()
         }
         .padding()
     }
